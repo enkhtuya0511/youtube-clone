5f02fdc87bddaf585d56a17515b0a5b6fbbf295d
h0ngsae <enkhtuya.b511@gmail.com>
2023-10-11 12:03:34 +0800

    0

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
     <>
-      <main className="flex min-h-screen pt-[55px]">
+      <main className="flex min-h-screen pt-[55px] bg-[#212121]">
       <NavBar setMenu={setMenu} />
         {menu && <SideBar />}
         <div className='flex flex-col w-[85%]'>
